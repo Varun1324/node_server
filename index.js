6db568fe568fa9054a17d4e5dd13245c4e6b4cf5
Varun Kodamanchili <varun132413@gmail.com>
2025-01-06 20:25:20 +0530

    register server added

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{
-    res.send("server running")
+    console.log("Server Running");
 })
 
-app.listen(3000,()=>{console.log("server running on port 3000")})
+app.post("/register",(req,res)=>{
+    console.log(res);
+    const formdata = req.body;
+    console.log(formdata);
+    res.status(200).json({ message: 'successful!' });
+})  
 
-module.exports = app;
+app.listen(3000,()=>{
+    console.log("server running on port 3000");
+})
